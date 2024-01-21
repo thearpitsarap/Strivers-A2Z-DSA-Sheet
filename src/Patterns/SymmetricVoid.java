d02105538c6d46755a4ab78a298d9aefe78d3258
thearpitsarap <arpitsarap18@gmail.com>
2024-01-22 03:58:24 +0530

    symmetric void

---
+++ src/Patterns/SymmetricVoid.java
@@ -0,0 +1,37 @@
+package Patterns;
+
+public class SymmetricVoid {
+    public static void symmetry(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=n-i;j>=0;j--){
+                System.out.print("*");
+            }
+
+            for(int j=2*i-2;j>=1;j--){
+                System.out.print(" ");
+            }
+
+            for(int j=n-i;j>=0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=0;j<=n-i;j++){
+                System.out.print("*");
+            }
+
+            for(int j=1;j<=2*i-2;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=0;j<=n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        symmetry(3);
+    }
+}
