23dfb5edb34138de0e363cdffb9e2a51d585662f
thearpitsarap <arpitsarap18@gmail.com>
2024-01-22 04:56:19 +0530

    square start pattern

---
+++ src/Patterns/StartSquarePattern.java
@@ -0,0 +1,35 @@
+package Patterns;
+
+public class StartSquarePattern {
+    public static void getStarPattern(int n) {
+        for(int i=0;i<n;i++){
+            if(i==0){
+                for(int j=0;j<n;j++){
+                    System.out.print("*");
+                }
+            }
+            else if(i==n-1){
+                for(int j=0;j<n;j++){
+                    System.out.print("*");
+                }
+            }
+            else {
+                for(int j=0;j<n;j++){
+                    if(j==0){
+                        System.out.print("*");
+                    }
+                    else if(j==n-1){
+                        System.out.print("*");
+                    }
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        getStarPattern(3);
+    }
+}
