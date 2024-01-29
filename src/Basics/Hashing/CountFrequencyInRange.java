acbc61630e2551ce19909d0f9a07bbe177554aa9
thearpitsarap <arpitsarap18@gmail.com>
2024-01-29 19:07:53 +0530

    count frequency using hashing

---
+++ src/Basics/Hashing/CountFrequencyInRange.java
@@ -0,0 +1,21 @@
+package Basics.Hashing;
+
+public class CountFrequencyInRange {
+    public static int[] countFrequency(int n, int x, int []nums){
+        int[] hash = new int[x+1];
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;    
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=hash[i+1];
+            System.out.println(arr[i]);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 1, 9, 2, 7};
+        System.out.println(countFrequency(6, 9, arr));
+    }
+}
