84651c9a607277434518e644f05a5624f65033f8
thearpitsarap <arpitsarap18@gmail.com>
2024-01-30 18:16:17 +0530

    insertion sort

---
+++ src/SortingTechniques/InsertionSort.java
@@ -0,0 +1,24 @@
+package SortingTechniques;
+
+public class InsertionSort {
+    public static int[] selectionSort(int[] arr) {
+        for(int i=0;i<=arr.length;i++){
+            for(int j=i-1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    int t = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1]=t;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String args[]){
+        int[] arr = {2, 13, 4, 1, 3, 6, 28};
+        int[] s = selectionSort(arr);
+        for(int i=0;i<s.length;i++){
+            System.out.println(s[i]);
+        }
+    }
+}
