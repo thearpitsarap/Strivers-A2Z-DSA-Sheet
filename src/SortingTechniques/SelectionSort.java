2531700626e0fb81a1f4527fe4c7354a01ad08a5
thearpitsarap <arpitsarap18@gmail.com>
2024-01-30 16:14:59 +0530

    selection sort

--- src/SortingTechniques/SelectionSort.java
+++ src/SortingTechniques/SelectionSort.java
@@ -2,7 +2,7 @@ package SortingTechniques;
 
 public class SelectionSort {
     public static int[] selectionSort(int[] arr) {
-        for(int i=0;i<arr.length-1;i++){
+        for(int i=0;i<arr.length;i++){
             int x = i;
             for(int j=i+1;j<arr.length;j++){
              if(arr[j]<arr[x]){
