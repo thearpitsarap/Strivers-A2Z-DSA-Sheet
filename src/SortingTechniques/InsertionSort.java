605ccf3650750c0c56dc9e134ae681b3ae3a9057
thearpitsarap <arpitsarap18@gmail.com>
2024-01-31 16:43:32 +0530

    merge sort

--- src/SortingTechniques/InsertionSort.java
+++ src/SortingTechniques/InsertionSort.java
@@ -1,7 +1,7 @@
 package SortingTechniques;
 
 public class InsertionSort {
-    public static int[] selectionSort(int[] arr) {
+    public static int[] insertionSort(int[] arr) {
         for(int i=0;i<=arr.length;i++){
             for(int j=i-1;j>0;j--){
                 if(arr[j]<arr[j-1]){
@@ -16,7 +16,7 @@ public class InsertionSort {
 
     public static void main(String args[]){
         int[] arr = {2, 13, 4, 1, 3, 6, 28};
-        int[] s = selectionSort(arr);
+        int[] s = insertionSort(arr);
         for(int i=0;i<s.length;i++){
             System.out.println(s[i]);
         }
