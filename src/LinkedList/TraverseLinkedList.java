48303aeae655a7f283c1cad404cc889f65a56bc3
thearpitsarap <arpitsarap18@gmail.com>
2024-02-16 15:42:01 +0530

    Started Linked List

---
+++ src/LinkedList/TraverseLinkedList.java
@@ -0,0 +1,51 @@
+package LinkedList;
+
+class Node {
+    public int data;
+    public Node next;
+
+    Node()
+    {
+        this.data = 0;
+        this.next = null;
+    }
+
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+
+    Node(int data, Node next)
+    {
+        this.data = data;
+        this.next = next;
+    }
+};
+
+
+public class TraverseLinkedList {
+    public static Node constructLL(int []nums) {
+        Node y = new Node(nums[0]);
+        Node mover =y;
+        for(int i=1;i<nums.length;i++){
+            Node x = new Node(nums[i]);
+            mover.next = x;
+            mover = x;
+        }
+        return y;
+    } 
+    
+    public static void ll(){
+        int[] nums  = {4, 2, 5, 1};
+        Node temp = constructLL(nums);
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ll();
+    }
+}
