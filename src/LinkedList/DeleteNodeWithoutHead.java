a7dfbac75c01305de1d5f22689cde883f9a27578
thearpitsarap <arpitsarap18@gmail.com>
2024-02-16 19:05:43 +0530

    delete node without knowing head node

---
+++ src/LinkedList/DeleteNodeWithoutHead.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+public class DeleteNodeWithoutHead {
+
+    public static void deleteNode(Node node) {
+        node.data = node.next.data;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+        CreateLinkedList cl = new CreateLinkedList();
+        TraverseLinkedList tl = new TraverseLinkedList();
+
+        int[] nums = {4,5,1,9};
+        Node temp = cl.constructLL(nums);
+        deleteNode(temp);
+        tl.traverse(temp);
+    }
+}
