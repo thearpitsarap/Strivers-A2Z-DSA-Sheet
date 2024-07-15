f901d8f7df85c506472c97df65d353ce652882c1
thearpitsarap <arpitsarap18@gmail.com>
2024-07-15 16:44:08 +0530

    dfs

---
+++ src/BinaryTrees/Traversals/DFS.java
@@ -0,0 +1,42 @@
+package BinaryTrees.Traversals;
+
+import java.util.*;
+
+class Node{
+    int val;
+    Node left,right;
+    
+    Node(int v){
+        val=v;
+        left=null;
+        right=null;
+    }
+}
+
+public class DFS {
+    
+    public static void trav(Node root){
+        Stack<Node> st = new Stack();
+        st.push(root);
+        while(!st.isEmpty()){
+            System.out.println(st.peek().val);
+            Node curr = st.pop();
+            if(curr.right!=null || curr.left!=null) {
+             st.push(curr.right);
+             st.push(curr.left);   
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Node head = new Node(55);
+        head.left = new Node(44);
+        head.left.left = new Node(77);
+        head.left.right = new Node(99);
+        head.right = new Node(88);
+        head.right.left = new Node(65);
+        head.right.right = new Node(78);
+        
+        trav(head);
+    }
+}
--- src/BinaryTrees/Traversals/Traversals.java
+++ src/BinaryTrees/Traversals/Traversals.java
@@ -1,5 +1,24 @@
 package BinaryTrees.Traversals;
 
+class Node{
+    int key;
+    Node left;
+    Node right;
+
+    Node(int key){
+        this.key=key;
+        left = right = null;
+    }
+}
+
 public class Traversals {
-    
+    public static void main(String[] args) {
+        Node head = new Node(55);
+        head.left = new Node(44);
+        head.right = new Node(88);
+        
+        System.out.println(head.key);
+        System.out.println(head.left.key);
+        System.out.println(head.right.key);
+    }
 }
