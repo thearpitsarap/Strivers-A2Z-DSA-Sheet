1af5ae7c71dcb09cf652e23fc4f41ee37005d713
thearpitsarap <arpitsarap18@gmail.com>
2025-02-18 13:58:47 +0530

    ‘a’ followed by 4 to 8 'b’s

---
+++ src/PlacementPractice/StringQue.java
@@ -0,0 +1,15 @@
+package PlacementPractice;
+
+public class StringQue {
+    public static void main(String[] args) {
+        String s = "abbbbabbbbbbbb";
+        int c = 0;
+        String[] arr = s.split("a");
+        for(int i=1;i<arr.length;i++){
+            if(arr[i].length()==4 || arr[i].length()==8){
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
