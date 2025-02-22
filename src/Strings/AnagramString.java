dd78e5c23c7e259fef86a81be6f07f0f2c084b4b
thearpitsarap <arpitsarap18@gmail.com>
2025-02-22 11:57:31 +0530

    anagram string

---
+++ src/Strings/AnagramString.java
@@ -0,0 +1,20 @@
+public class AnagramString {
+    public static void main(String[] args) {
+        String s1 = "asar";
+        String s2 = "asat";
+        
+        Map<Character,Integer> map = new HashMap();
+        if(s1.length()!=s2.length()){
+            System.out.println("False");
+            return;
+        }
+        for(int i=0;i<s1.length();i++){
+            map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0) + 1);
+            map.put(s2.charAt(i), map.getOrDefault(s2.charAt(i), 0) - 1);
+        }
+        Set<Character> set = map.keySet();
+        for(Character a:set){
+            System.out.println(a +" "+map.get(a));
+        }
+    }
+}
