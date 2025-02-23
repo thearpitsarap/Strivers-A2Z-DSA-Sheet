db8d58f40b20fc1caf030b8b844f4c47e1e00b18
thearpitsarap <arpitsarap18@gmail.com>
2025-02-23 10:38:11 +0530

    factory pattern

--- src/Strings/Pallandrome.java
+++ src/Strings/Pallandrome.java
@@ -2,18 +2,20 @@ package Strings;
 
 public class Pallandrome {
     public static void main(String[] args) {
-        String s = "saas";        
+        String s = "saas";
 
         int l = 0;
-        int r = s.length()-1;
-        
-        while(l<=r){
-            while(l<=r && !Character.isLetterOrDigit(s.charAt(l))) l++;
-            while(l<=r && !Character.isLetterOrDigit(s.charAt(r))) r++;
-                 
-                        if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r)) ){
-                 System.out.println(false);
-                            return;
+        int r = s.length() - 1;
+
+        while (l <= r) {
+            while (l <= r && !Character.isLetterOrDigit(s.charAt(l)))
+                l++;
+            while (l <= r && !Character.isLetterOrDigit(s.charAt(r)))
+                r++;
+
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                System.out.println(false);
+                return;
             }
             l++;
             r--;
