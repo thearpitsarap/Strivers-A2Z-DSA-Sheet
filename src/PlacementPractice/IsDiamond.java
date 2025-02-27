fd28a3702b4e71abc3a8cee726dc1567cc4804ae
thearpitsarap <arpitsarap18@gmail.com>
2025-02-27 16:02:52 +0530

    added

---
+++ src/PlacementPractice/IsDiamond.java
@@ -0,0 +1,30 @@
+package PlacementPractice;
+
+class Parent {
+    public void display(){
+        System.out.println("Parent");
+    }
+}
+
+interface MyInterface {
+    default public void display(){
+        System.out.println("Inter");
+    }
+}
+
+class IsDiamond1 extends Parent implements MyInterface{
+    @Override
+    public void display(){
+        System.out.println("IsDiamond");
+    }
+}
+
+public class IsDiamond{
+
+    public static void main(String[] args) {
+        IsDiamond1 parent = new IsDiamond1();
+        parent.display();
+
+        
+    }
+}
